856083fc04f586ff94dc8a71027f422c4f00b232
Viska97 <m.viska97@gmail.com>
2019-04-01 17:51:42 +0300

    Added tests and test data

---
+++ jcase/js_test/loader_test.js
@@ -0,0 +1,46 @@
+/**
+ * Модуль unit тестирования модуля загрузки.
+ */
+
+/**
+ * Тестирование функции loadJson(file).
+ */
+describe("loadJson(file)", function(done) {
+
+    it('should return result for valid json file', function() {
+        var mock = mockFile("./test_data/loader_true.json")
+        return loadJson(mock)
+            .then(function(result) { assert(true) })
+            .catch(function(error) { assert.fail('Error not expected') })
+    });
+
+
+    it('should return error for invalid json file', function() {
+        var mock = mockFile("./test_data/loader_false.json")
+        var expectedError = 'Загруженный файл не содержит структуру JSON'
+        return loadJson(mock)
+            .then(function(result) { assert.fail('Successful result not expected') })
+            .catch(function(error) { expect(error).to.equal(expectedError) })
+    });
+  
+});
+
+/**
+ * Функция: mockFile(path).
+ * 
+ * Функция для создания заглушки файла.
+ * 
+ * Параметры:
+ * 
+ * path - путь к файлу с тестовыми данными
+ * 
+ * Вовзращаемый результат:
+ * 
+ * объект, описывающий файл.
+ */
+function mockFile(path) {
+    var request = new XMLHttpRequest();
+    request.open("GET", path, false);
+    request.send(null)
+    return new File([request.responseText], "test.json");
+}
